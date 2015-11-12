b43da6089dc258bf2e17de559adbe86c2cd9b5aa
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-11-12 13:57:27 +0100

    add h2 dependency and configuration

---
+++ src/main/java/com/dawidkotarba/playground/configuration/Configuration.java
@@ -0,0 +1,19 @@
+package com.dawidkotarba.playground.configuration;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by Dawid Kotarba on 12.11.2015.
+ */
+@org.springframework.context.annotation.Configuration
+public class Configuration {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/h2/*");
+        return registrationBean;
+    }
+}
--- src/test/java/com/dawidkotarba/PlaygroundApplicationTests.java
+++ src/test/java/com/dawidkotarba/PlaygroundApplicationTests.java
@@ -1,5 +1,6 @@
 package com.dawidkotarba;
 
+import com.dawidkotarba.playground.PlaygroundApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
