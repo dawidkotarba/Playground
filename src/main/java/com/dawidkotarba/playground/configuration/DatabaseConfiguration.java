cbad2aee60c6f3061d1731587bb90c40169987f0
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-11-12 14:37:43 +0100

    automatic run of db_init script

--- src/main/java/com/dawidkotarba/playground/configuration/DatabaseConfiguration.java
+++ src/main/java/com/dawidkotarba/playground/configuration/DatabaseConfiguration.java
@@ -3,17 +3,28 @@ package com.dawidkotarba.playground.configuration;
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 /**
  * Created by Dawid Kotarba on 12.11.2015.
  */
 @org.springframework.context.annotation.Configuration
-public class Configuration {
+public class DatabaseConfiguration {
 
     @Bean
     ServletRegistrationBean h2servletRegistration() {
         ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
         registrationBean.addUrlMappings("/db/*");
         return registrationBean;
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder().
+                setType(EmbeddedDatabaseType.H2).
+                addScript("db_init.sql").
+                build();
     }
 }
