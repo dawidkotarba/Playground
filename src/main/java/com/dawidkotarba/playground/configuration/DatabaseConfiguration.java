2e3d8b3f19c1a9f5041fd116c22995caf991bbc4
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-11-12 18:05:11 +0100

    split db script into create and data init

--- src/main/java/com/dawidkotarba/playground/configuration/DatabaseConfiguration.java
+++ src/main/java/com/dawidkotarba/playground/configuration/DatabaseConfiguration.java
@@ -6,14 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.DatabasePopulator;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
@@ -36,8 +32,11 @@ public class DatabaseConfiguration {
 
     private static final String H2_JDBC_URL_TEMPLATE = "jdbc:h2:~/test";
 
-    @Value("classpath:db_init.sql")
-    private Resource H2_DATA_INIT_SCRIPT;
+    @Value("classpath:db_create.sql")
+    private Resource H2_DB_CREATE_SCRIPT;
+
+    @Value("classpath:db_data_init.sql")
+    private Resource H2_DB_DATA_INIT_SCRIPT;
 
     @Bean
     public DataSource dataSource(Environment env) throws Exception {
@@ -64,7 +63,8 @@ public class DatabaseConfiguration {
 
     private DatabasePopulator databasePopulator() {
         final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-        populator.addScript(H2_DATA_INIT_SCRIPT);
+        populator.addScript(H2_DB_CREATE_SCRIPT);
+        populator.addScript(H2_DB_DATA_INIT_SCRIPT);
         return populator;
     }
 
