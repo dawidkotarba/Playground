56c5c7439cfc2ef1edb940fd02f4b4a4f77637b5
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-11-12 16:23:36 +0100

    update db config

--- src/main/java/com/dawidkotarba/playground/configuration/DatabaseConfiguration.java
+++ src/main/java/com/dawidkotarba/playground/configuration/DatabaseConfiguration.java
@@ -1,22 +1,48 @@
 package com.dawidkotarba.playground.configuration;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.h2.server.web.WebServlet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Created by Dawid Kotarba on 12.11.2015.
  */
 @Configuration
-@ComponentScan("com.dawidkotarba.playground.model.entities")
 @EnableTransactionManagement
 public class DatabaseConfiguration {
+
+    private static final String H2_JDBC_URL_TEMPLATE = "jdbc:h2:~/test";
+
+    @Value("classpath:db_init.sql")
+    private Resource H2_DATA_INIT_SCRIPT;
+
+    @Bean
+    public DataSource dataSource(Environment env) throws Exception {
+        return createH2DataSource();
+    }
 
     @Bean
     ServletRegistrationBean h2servletRegistration() {
@@ -25,11 +51,64 @@ public class DatabaseConfiguration {
         return registrationBean;
     }
 
-    @Bean(destroyMethod = "shutdown")
-    public EmbeddedDatabase dataSource() {
-        return new EmbeddedDatabaseBuilder().
-                setType(EmbeddedDatabaseType.H2).
-                addScript("db_init.sql").
-                build();
+    @Autowired
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
+    }
+
+
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(H2_DATA_INIT_SCRIPT);
+        return populator;
+    }
+
+    private DataSource createH2DataSource() {
+        String jdbcUrl = String.format(H2_JDBC_URL_TEMPLATE, System.getProperty("user.dir"));
+        JdbcDataSource ds = new JdbcDataSource();
+        ds.setURL(jdbcUrl);
+        ds.setUser("sa");
+        ds.setPassword("");
+
+        return ds;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(Environment env) throws Exception {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(Boolean.TRUE);
+        vendorAdapter.setShowSql(Boolean.TRUE);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("com.dawidkotarba.playground.model.entities");
+        factory.setDataSource(dataSource(env));
+
+        factory.setJpaProperties(jpaProperties());
+        factory.setLoadTimeWeaver(new InstrumentationLoadTimeWeaver());
+
+        return factory;
+    }
+
+    Properties jpaProperties() {
+        Properties props = new Properties();
+        props.put("hibernate.query.substitutions", "true 'Y', false 'N'");
+        props.put("hibernate.hbm2ddl.auto", "create-drop");
+        props.put("hibernate.show_sql", "false");
+        props.put("hibernate.format_sql", "true");
+
+        return props;
     }
 }
