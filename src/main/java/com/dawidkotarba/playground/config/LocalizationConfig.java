93ff140bb8727b31a44a5c2f97f678e8d292e1c1
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-11-14 21:43:08 +0100

    update localization config and service

--- src/main/java/com/dawidkotarba/playground/config/LocalizationConfig.java
+++ src/main/java/com/dawidkotarba/playground/config/LocalizationConfig.java
@@ -1,5 +1,6 @@
 package com.dawidkotarba.playground.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import javax.annotation.PostConstruct;
 import java.util.Locale;
 
 /**
@@ -18,10 +20,20 @@ import java.util.Locale;
 @Configuration
 public class LocalizationConfig extends WebMvcConfigurerAdapter {
 
+    @Value("${default.locale}")
+    private String defaultLocaleStr;
+
+    private Locale defaultLocale;
+
+    @PostConstruct
+    public void init() {
+        defaultLocale = new Locale.Builder().setLanguageTag(defaultLocaleStr).build();
+    }
+
     @Bean
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.US);
+        slr.setDefaultLocale(defaultLocale);
         return slr;
     }
 
--- src/main/java/com/dawidkotarba/playground/service/i18n/LocalizationService.java
+++ src/main/java/com/dawidkotarba/playground/service/i18n/LocalizationService.java
@@ -1,12 +1,9 @@
 package com.dawidkotarba.playground.service.i18n;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import java.util.Locale;
 
 /**
  * Created by Dawid Kotarba on 14.11.2015.
@@ -18,22 +15,12 @@ public class LocalizationService {
     @Autowired
     private MessageSource messageSource;
 
-    @Value("${default.locale}")
-    private String defaultLocaleStr;
-
-    private Locale defaultLocale;
-
-    @PostConstruct
-    public void init() {
-        defaultLocale = new Locale.Builder().setLanguageTag(defaultLocaleStr).build();
-    }
-
     public String getMessage(String code) {
-        return messageSource.getMessage(code, null, defaultLocale);
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
     }
 
     public String getMessage(String code, Object[] args) {
-        return messageSource.getMessage(code, args, defaultLocale);
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 
 }
