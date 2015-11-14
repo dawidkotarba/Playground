c6e5d67f6c797b68f831c9785cbf610d4ef39e5a
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-11-14 23:06:24 +0100

    update config

--- src/main/java/com/dawidkotarba/playground/config/GeneralConfig.java
+++ src/main/java/com/dawidkotarba/playground/config/GeneralConfig.java
@@ -2,11 +2,15 @@ package com.dawidkotarba.playground.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by Dawid Kotarba on 12.11.2015.
  */
 @Configuration
 @ComponentScan("com.dawidkotarba.playground")
+@EnableAsync
+@EnableScheduling
 public class GeneralConfig {
 }
