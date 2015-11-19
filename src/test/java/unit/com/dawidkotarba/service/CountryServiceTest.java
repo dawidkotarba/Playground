9add091c873426603d65d5b6cb812fe53777283f
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-11-19 20:40:53 +0100

    update Country Service test

--- src/test/java/unit/com/dawidkotarba/service/CountryServiceTest.java
+++ src/test/java/unit/com/dawidkotarba/service/CountryServiceTest.java
@@ -4,10 +4,7 @@ import com.dawidkotarba.playground.dao.CountryDao;
 import com.dawidkotarba.playground.integration.dto.CountryDto;
 import com.dawidkotarba.playground.service.CountryService;
 import integration.com.dawidkotarba.AbstractTestNgConfiguration;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.*;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -17,6 +14,8 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
@@ -45,7 +44,30 @@ public class CountryServiceTest extends AbstractTestNgConfiguration {
         // when
         List<CountryDto> result = underTest.getAll();
 
+        // then
         assertThat(result, is(notNullValue()));
         verify(countryDao, Mockito.times(1)).getAll();
+    }
+
+    @Test
+    public void getByNameTest() {
+        // given
+        doReturn(new ArrayList<CountryDto>()).when(countryDao).getByName(anyString());
+
+        // when
+        List<CountryDto> result = underTest.getByName("test");
+
+        // then
+        assertThat(result, is(notNullValue()));
+        verify(countryDao, Mockito.times(1)).getByName(anyString());
+    }
+
+    @Test
+    public void addTest() {
+        // when
+        underTest.add(new CountryDto());
+
+        // then
+        verify(countryDao, Mockito.times(1)).add(any());
     }
 }
