6529a86419e618e3d72198dfa1e75b670e68b0fd
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-11-20 20:55:24 +0100

    replace properties by yaml. Add Async Config

--- src/main/java/com/dawidkotarba/playground/config/GeneralConfig.java
+++ src/main/java/com/dawidkotarba/playground/config/GeneralConfig.java
@@ -1,16 +1,14 @@
 package com.dawidkotarba.playground.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by Dawid Kotarba on 12.11.2015.
  */
 @Configuration
 @ComponentScan("com.dawidkotarba.playground")
-@EnableAsync
-@EnableScheduling
+@EnableConfigurationProperties
 public class GeneralConfig {
 }
