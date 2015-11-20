fa0ad05ac78f93705aae123c9d9004221f9a36a2
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-11-20 21:19:19 +0100

    remove context config from testNg configuration

--- src/test/java/integration/com/dawidkotarba/AbstractTestNgConfiguration.java
+++ src/test/java/integration/com/dawidkotarba/AbstractTestNgConfiguration.java
@@ -1,12 +1,8 @@
 package integration.com.dawidkotarba;
 
 import com.dawidkotarba.playground.PlaygroundApplication;
-import com.dawidkotarba.playground.config.DatabaseConfig;
-import com.dawidkotarba.playground.config.GeneralConfig;
-import com.dawidkotarba.playground.config.LocalizationConfig;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
@@ -19,7 +15,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringApplicationConfiguration(classes = PlaygroundApplication.class)
 @WebAppConfiguration
-@ContextConfiguration(classes = {DatabaseConfig.class, GeneralConfig.class, LocalizationConfig.class})
 @IntegrationTest
 @TestExecutionListeners(inheritListeners = false, listeners = {
         DependencyInjectionTestExecutionListener.class,
