f69510e8f8b5d389fdb085bbdb37bee5f9e065e0
Dawid Kotarba <dawid.kotarba@gmail.com>
2015-12-28 21:22:40 +0100

    add authentication logger config

---
+++ src/main/java/com/dawidkotarba/playground/config/AuditEventLogConfiguration.java
@@ -0,0 +1,22 @@
+package com.dawidkotarba.playground.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.event.AbstractAuthenticationEvent;
+
+/**
+ * Created by Dawid Kotarba on 28.12.2015.
+ */
+
+@Configuration
+public class AuditEventLogConfiguration implements ApplicationListener<AbstractAuthenticationEvent> {
+
+    private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void onApplicationEvent(AbstractAuthenticationEvent event) {
+        LOGGER.info("Authentication event: " + event);
+    }
+}
