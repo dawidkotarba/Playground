2e033d8f1390bb12b57e529d251c068c517caf74
Dawid Kotarba <dawid.kotarba@gmail.com>
2016-01-17 00:57:17 +0100

    add hateoas self links for country ctrl methods

--- src/main/java/com/dawidkotarba/playground/controllers/CountryController.java
+++ src/main/java/com/dawidkotarba/playground/controllers/CountryController.java
@@ -3,15 +3,20 @@ package com.dawidkotarba.playground.controllers;
 import com.dawidkotarba.playground.integration.dto.CountryDto;
 import com.dawidkotarba.playground.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * Created by Dawid Kotarba on 12.11.2015.
@@ -29,13 +34,29 @@ public class CountryController {
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<CountryDto> all() {
-        return countryService.getAll();
+    public List<Resource<CountryDto>> getAll() {
+        List<Resource<CountryDto>> result = new ArrayList<>();
+
+        for (CountryDto dto : countryService.getAll()) {
+            Resource resource = new Resource(dto);
+            resource.add(linkTo(methodOn(CountryController.class).getByName(dto.getName())).withSelfRel());
+            result.add(resource);
+        }
+
+        return result;
     }
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<CountryDto> getByName(@PathVariable String name) {
-        return countryService.getByName(name);
+    public List<Resource<CountryDto>> getByName(@PathVariable String name) {
+        List<Resource<CountryDto>> result = new ArrayList<>();
+
+        for (CountryDto dto : countryService.getByName(name)) {
+            Resource resource = new Resource(dto);
+            resource.add(linkTo(methodOn(CountryController.class).getByName(dto.getName())).withSelfRel());
+            result.add(resource);
+        }
+
+        return result;
     }
 
     @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
