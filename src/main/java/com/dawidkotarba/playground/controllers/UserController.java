d7bd5c2d32568939ef90b48dd4c46145e1db6fb3
Dawid Kotarba <dawid.kotarba@gmail.com>
2016-01-17 01:01:48 +0100

    add hateoas self links for user ctrl methods

--- src/main/java/com/dawidkotarba/playground/controllers/UserController.java
+++ src/main/java/com/dawidkotarba/playground/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.dawidkotarba.playground.integration.dto.UserInDto;
 import com.dawidkotarba.playground.integration.dto.UserOutDto;
 import com.dawidkotarba.playground.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * Created by Dawid Kotarba on 15.11.2015.
@@ -30,13 +35,30 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<UserOutDto> getAll() {
-        return userService.getAll();
+    public List<Resource<UserOutDto>> getAll() {
+        List<Resource<UserOutDto>> result = new ArrayList<>();
+
+        for (UserOutDto dto : userService.getAll()) {
+            Resource resource = new Resource(dto);
+            resource.add(linkTo(methodOn(UserController.class).getByName(dto.getUsername())).withSelfRel());
+            result.add(resource);
+        }
+
+        return result;
     }
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<UserOutDto> getByName(@PathVariable String name) {
-        return userService.getByName(name);
+    public List<Resource<UserOutDto>> getByName(@PathVariable String name) {
+
+        List<Resource<UserOutDto>> result = new ArrayList<>();
+
+        for (UserOutDto dto : userService.getByName(name)) {
+            Resource resource = new Resource(dto);
+            resource.add(linkTo(methodOn(UserController.class).getByName(dto.getUsername())).withSelfRel());
+            result.add(resource);
+        }
+
+        return result;
     }
 
     @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
